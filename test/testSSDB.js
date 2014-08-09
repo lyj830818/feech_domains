2fb9cc0da474f200cf615abff41a61a5936c0622
lyj <29422882@qq.com>
2014-08-09 09:20:28 +0800

    ci

--- test/testSSDB.js
+++ test/testSSDB.js
@@ -4,16 +4,20 @@
 
 
 
-var config = require('../config').aliyun,
+var config = require('../config').vbox,
 SSDB = require('../src/SSDB'),
 rHash = require('../src/RemoteHash');
 
 
 var ssdbClient,remoteHash;
 
+
+console.dir(config)
 ssdbClient = SSDB.connect(config.ssdb.host, config.ssdb.port);
+
 remoteHash = new rHash.RemoteHash(ssdbClient, {'serverType': 'ssdb', 'mainKey': 'aaa'});
 
+/*
 setTimeout(function(){
   remoteHash.delMainKey(done);
 }, 2000);
@@ -21,6 +25,7 @@ setTimeout(function(){
 setTimeout(function(){
   remoteHash.get('key2' ,done);
 }, 4000);
+*/
 
 var done = function(err, reply){
   console.dir(err);
@@ -30,8 +35,12 @@ var done = function(err, reply){
 //remoteHash.set('key2', 'val2', done);
 //remoteHash.exists('key2', done);
 
+var val = [{"url":"http://www.haonai365.com/items/Jftf.html?id=491&cid=01"}];
+val = JSON.stringify(val);
 
-//ssdbClient.qpush('queue' , 'sodofo', done);
-//ssdbClient.qpop_front('queue', done);
-//ssdbClient.qsize('task_queue_key', done);
+
+//ssdbClient.qpush('task_queue_key', val, done);
+//ssdbClient.qpop_front('task_queue_key', done);
+ssdbClient.qsize('task_queue_key', done);
 //ssdbClient.qclear('queue', done);
+//
